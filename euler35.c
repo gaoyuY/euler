9c783e8228ed1aacf0b821d6449b302a51736617
gaoyuY <2282940822@qq.com>
2018-08-14 23:08:30 +0800

    update code

--- euler35.c
+++ euler35.c
@@ -4,33 +4,38 @@
 	> Mail: 
 	> Created Time: 2018年07月28日 星期六 14时27分59秒
  ************************************************************************/
-
+/*题目　圆周素数
+ * 197被称为圆周素数，因为将它逐位旋转所得到的数：197/971和719都是素数。
+ * 小于100的圆周素数有十三个：2、3、5、7、11、13、17、31、37、71、73、79和97。
+ * 小于一百万的圆周素数有多少个？
+ * 答案：55
+ */
 #include<stdio.h>
 #include<math.h>
 #define MAX_N 1000000
 int prime[MAX_N + 5] = {0};
 
-int Prime(int x) {
+int Prime(int x) {   //判断是否为素数
     
     for (int i = 2; i * i <= x; i++) {
-                if (x % i == 0)  return 0;
+            if (x % i == 0)  return 0;
             
     }
-        return 1;
+        return 1; // 是素数返回１
 }
 
 int main () {
     int ans = 0;
     int n;
     int m;
-    int a;
+    int a; 
     for (int i = 2; i < MAX_N; i++) {
-        n = floor(log10(i) + 1);
+        n = floor(log10(i) + 1); //求位数
         m = n;
         a = i; 
         while (m) {
             m--;
-            if (Prime(a)) {
+            if (Prime(a)) { //判断这个数是否为圆周素数
                 if (m == 0) {
                     ans ++;
                    // printf ("%d ", a);
--- euler37.c
+++ euler37.c
@@ -4,19 +4,25 @@
 	> Mail: 
 	> Created Time: 2018年07月28日 星期六 19时50分00秒
  ************************************************************************/
-
+/*题目可截素数 3797有着奇特的性质。不仅它本身是一个素数，而且如果从左往右逐一截去数字，剩下的仍然都是素数：3797、797、97和7；
+ * 同样地，如果从右往左逐一截去数字，剩下的也依然都是素数：3797、379、37和3。
+ * 只有11个素数，无论从左往右还是从右往左逐一截去数字，剩下的仍然都是素数，求这些数的和。
+ * 注意：2、3、5和7不被视为可截素数
+ * 答案：748317
+ * */
 #include<stdio.h>
 #include<inttypes.h>
 #include<math.h>
 #define MAX_N 1000000
-//#define Dug 
+//#define Dug      //自定义输出 
 #ifdef Dug
 #define dug1(a, b) printf(a, b)
 #else 
 #define dug1(a, b)
 #endif
-int prime[MAX_N] = {0};
-int is_prime[MAX_N] = {0};
+int prime[MAX_N] = {0};     //存放素数的数组
+int is_prime[MAX_N] = {0};  //判断是否为素数
+ //素数筛
 void Prime() {
     is_prime[1] = 1;
     is_prime[0] = 1;
@@ -34,8 +40,8 @@ void Prime() {
 }
 
 
-int han1(int a){
-    int c = floor(log10(a) + 1);
+int han1(int a){  // 正向截取判断是否是素数
+    int c = floor(log10(a) + 1);//求位数
     while (!is_prime[a]) {
         a %= (int)pow(10, --c);
         if (c == 0) return 1;
@@ -43,7 +49,7 @@ int han1(int a){
     return 0;
 }  
 
-int han2(int b) {
+int han2(int b) {    //反向截取判断是否是素数
     while (!is_prime[b]) {
         b /= 10;
     }
@@ -54,11 +60,11 @@ int main () {
     Prime();
     int n = 11;
     int sum = 0;
-    for (int i = 11; n != 0; i++){
+    for (int i = 11; n != 0; i++){ //因为只有11个这样的数，就以11作为跳出条件
         if (han1(i) && han2(i)){
-            sum = sum + i;
+            sum = sum + i;         //求和
             n--;
-           dug1("%d\n", i);
+           //dug1("%d\n", i);
         }
 
     }
